e1df3b1483c5fb5292e3997ac758ac7c8bdb75e5
MonteC73 <jarekucinski@gmail.com>
2018-02-26 00:14:45 +0100

    git-card-component

--- git-viewer/src/App.js
+++ git-viewer/src/App.js
@@ -2,17 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Header = (props) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    </div>
+  )
+}
+
+const Card = (props) => {
+  return (
+    <div style={{margin: '1em'}}>
+      <img width="75" src={props.avatar_url} />
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1em', fontWeight: 'bold'}} >
+          {props.name}
+        </div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+  );
+};
+
+let data = [
+  { name: "Paul O'Shannessy",
+    avatar_url: "https://avatars.githubusercontent.com/u/8445?v3",
+    company: "Facebook" },
+    { name: "Paul O'Shannessy",
+    avatar_url: "https://avatars.githubusercontent.com/u/8445?v3",
+    company: "Facebook" },
+    { name: "Paul O'Shannessy",
+    avatar_url: "https://avatars.githubusercontent.com/u/8445?v3",
+    company: "Facebook" }
+];
+
+const CardList = (props) => {
+  return (
+    <div>
+      {props.cards.map(card => <Card {...card} />)}
+    </div>
+  );
+};
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <CardList cards={data}/>
       </div>
     );
   }
